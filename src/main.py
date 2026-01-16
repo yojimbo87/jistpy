edb2f0b39a1ddb13552571fa03c1ac01edbca7e6
yojimbo87 <bosak.tomas@gmail.com>
2026-01-17 00:08:07 +0100

    Implement parsing of formula into forest components

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from jist import JIST
 from utils import Secret
-from pprint import pprint
+from devtools import pprint
 
 secret = Secret("secret.ini", "Credentials2")
 
@@ -10,4 +10,4 @@ jist = JIST(secret.hostname, secret.username, secret.password)
 #data = jist.get_structure(600)
 data = jist.get_forest(600)
 
-pprint(data)
+pprint(data.components)
--- src/specs/forest_spec.py
+++ src/specs/forest_spec.py
@@ -9,8 +9,19 @@ class Version(BaseModel):
     signature: int
     version: int
 
+class ForestComponent(BaseModel):
+    raw_component: str
+    row_id: int
+    row_depth: int
+    item_identity: str
+    row_semantic: int
+    item_type: int
+    item_id: str
+    issue_id: int
+
 class Forest(BaseModel):
     spec: ForestSpec
     formula: str
+    components: list[ForestComponent] = Field(default=None)
     item_types: dict = Field(alias = "itemTypes")
     version: Version
